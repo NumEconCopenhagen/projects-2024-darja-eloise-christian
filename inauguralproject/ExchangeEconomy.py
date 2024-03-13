84d96193ae8e38666c1e11f7c21503023199765b
darja2502 <czs927@alumni.ku.dk>
2024-03-13 15:08:34 +0100

    add self

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -15,16 +15,16 @@ class ExchangeEconomyClass:
         par.w2A = 0.3
 
     def utility_A(self,x1A,x2A):
-        return x1A**(par.alpha)*x2A**(1-par.alpha)
+        return x1A**(self.par.alpha)*x2A**(1-self.par.alpha)
 
     def utility_B(self,x1B,x2B):
-        return x1B**(par.beta)*x2B**(1-par.beta)
+        return x1B**(self.par.beta)*x2B**(1-self.par.beta)
 
     def demand_A(self,p1):
-        return par.alpha * (p1*par.w1A + par.w2A)/p1 + (1-par.alpha) * p1*par.w1A + w2A
+        return par.alpha * (p1*self.par.w1A + self.par.w2A)/p1 + (1-self.par.alpha) * p1*self.par.w1A + w2A
 
     def demand_B(self,p1):
-        return par.beta * (p1*par.w1B + par.w2B)/p1 + (1-par.beta) *(p1*par.w1B + par.w2B)
+        return par.beta * (p1*self.par.w1B + self.par.w2B)/p1 + (1-self.par.beta) *(p1*self.par.w1B + self.par.w2B)
     
     # Check for Pareto improvements
     #def is_pareto_improvement(self, xA1, xA2, x1B, x2B):
