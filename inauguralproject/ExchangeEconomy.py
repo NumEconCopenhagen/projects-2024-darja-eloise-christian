e064d8980501ba86a41b67c3009a2a7c23d1875c
darja2502 <czs927@alumni.ku.dk>
2024-03-18 13:34:59 +0100

    ..

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -62,8 +62,9 @@ class ExchangeEconomyClass:
         return -(x1A**(self.par.alpha)*x2A**(1-self.par.alpha))
 
 
-    def aggregate_utility(self,x):
+    def aggregate_utility(self, x):
         xA1 = x[0]
         xA2 = x[1]
-        return -(self.utility_A(self,xA1, xA2) + self.utility_B(self, 1 - xA1, 1 - xA2))
+        return -(self.utility_A(xA1, xA2) + self.utility_B(1 - xA1, 1 - xA2))
+
     
